c38dcfda5a86b4b8b6f10b4990268ea709821049
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-06-15 19:57:05 +0300

    new changes added

---
+++ app/src/main/java/com/myapplication/mazejava/GameView.java
@@ -0,0 +1,20 @@
+package com.myapplication.mazejava;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class GameView extends View {
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.GREEN);
+    }
+}
